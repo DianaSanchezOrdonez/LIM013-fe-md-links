a4d5e69bf7b71a4c0135ff940c011ff46d9c842f
Diana Sánchez Ordóñez <dianaordonez1998@gmail.com>
2020-09-28 15:37:47 -0500

    validar ruta relativa o absoluta

--- src/index.js
+++ src/index.js
@@ -1,65 +1,37 @@
 #!/usr/bin/env node
-//import {createRequire} from 'module';
-//const require = createRequire(import.meta.url);
+
 import fs from 'fs';
+import MarkdownIt from 'markdown-it';
+import path from 'path';
+import {fileURLToPath} from 'url';
 //const http = require('http');
 //const request = require('request');  
 import process from 'process';
 
 let ruta;
+const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
 
 process.stdout.write('Ingresar la ruta:');
 process.stdin.on('data', function (data) {
   ruta = data.toString().trim();
-  process.stdout.write(`Validando ruta ${ruta}`);
-  existFile(ruta);
-}) 
-
-const existFile = () => {
-
-  fs.access(ruta, fs.constants.F_OK, (err) => {
-    if (err) {
-      throw ('No existe la ruta')
-    } else {
-      console.log('La ruta existe');
-    }
-    process.exit();
-  })
-}
-
-export default existFile; 
-
-/* function handler(req, res) {
-  request(ruta, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log('URL IS OK');
-      //res.writeHead(200, {'Content-Type':'text/html'})
-      res.end('Url is OK')
-    } else {
-      //res.writeHead(500, {'Content-Type': 'text/html'});
-      res.end('URL broke:' + JSON.stringify(response, null, 2));
-    }
-    process.exit()
-  })
-};
-
-http.createServer(handler).listen(3000);  */
-
-/* let ruta;
-process.stdout.write('Ingresar la ruta:');
-process.stdin.on('data', function (data) {
-  ruta = data.toString().trim();
-  process.stdout.write(`Validando ruta ${ruta}`);
-  existFile(ruta);
+  process.stdout.write(`Validando ruta `);
+  validPath(ruta);
 })
 
-const existFile = (ruta) => {
-  fs.access(ruta, fs.constants.F_OK, (err) => {
-    if (err) {
-      throw ('No existe la ruta')
-    } else {
-      console.log('La ruta existe');
-    }
-    process.exit();
-  })
-} */
+const validPath = (linkPath) => {
+  //ruta = ruta.toString()
+  if(!path.isAbsolute(ruta)){
+    linkPath = path.join(__dirname,linkPath)
+  }else{
+    console.log('la ruta es absoluta', linkPath);
+  }
+}
+
+//validPath('C:/DIANA/laboratoria/LIM013-fe-md-links')
+
+//const jsonPath = path.join(__dirname,ruta); 
+//const jsonString = fs.readFileSync(jsonPath, 'utf8');
+//console.log(jsonPath)
+//console.log(jsonString)
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,117 @@
-import prueba from './index'
+const fs = require('fs');
 
-prueba.existFile()
+const archivo = 'text.txt'
+
+/*---validar si existe sincrono---*/
+if (fs.existsSync(archivo)) {
+  console.log('el archivo existe');
+} else {
+  console.log('el archivo no existe');
+}
+
+/*---validar si existe asincrono---*/
+fs.access(archivo, fs.constants.F_OK, (err) => {
+  if (err) {
+    console.log('El archivo no existe');
+  } else {
+    console.log('El archivo existe');
+  }
+})
+
+/*---escribir en un archivo sincrono---*/
+const contenido = 'Manejando modulos de node.js';
+fs.writeFileSync(archivo, contenido);
+console.log('se ha escrito en el archivo');
+
+/*---escribir en un archivo asincrono---*/
+fs.writeFile(archivo, contenido, (err) => {
+  if (err) {
+    throw ('Hubo un error')
+  } else {
+    console.log('Se a escrito en el archivo');
+  }
+})
+
+const textoAdicional = '\nAqui va otra linea de codigo';
+fs.appendFile(archivo, textoAdicional, (err) => {
+  if (err) {
+    throw ('No se adjunto más texto', err)
+  } else {
+    'se agrego más texto'
+  }
+})
+
+fs.stat('README.md', function (err) {
+  if (!err) {
+    console.log('el directorio existe');
+  } else if (err.code === 'ENOENT') {
+    console.log('el directorio no existe');
+  }
+})
+
+/*---PROCESS WRITE Y ON ---*/
+let nombre;
+let preguntas = ['¿Cuál es tu nombre? ', '¿Cuál es tu edad?', '¿Lenguaje de programación favorito?'];
+let respuestas = [];
+
+function pregunta(i) {
+  process.stdout.write(preguntas[i]);
+}
+
+process.stdin.on('data', function (data) {
+  respuestas.push(data.toString().trim());
+
+  if (respuestas.length < preguntas.length) {
+    pregunta(respuestas.length)
+  } else {
+    process.exit();
+  }
+})
+
+pregunta(0);
+
+process.stdin.on('data', function (data) {
+  nombre = data.toString().trim();
+  process.stdout.write(`Hola ${nombre}!`);
+  process.exit();
+})
+
+/*---crear directorio---*/
+fs.mkdirSync('img');
+/*--- crear directorio asincrono---*/
+fs.mkdir('css', function (error) {
+  if (error) {
+    throw ('Error: ' + error)
+  }
+})
+/*--- validar si existe sincrono---*/
+if (fs.existsSync('css')) {
+  console.log('la carpeta ya existe');
+} else {
+  fs.mkdir('css', function (error) {
+    if (error) {
+      throw (error)
+    }
+    console.log('la carpeta ha sido creada');
+  });
+
+}
+
+/*--- leer archivo sincrono---*/
+let files = fs.readdirSync('./src')
+
+fs.readdir('./src', (error, files) => {
+  if (error) {
+    throw (error);
+  }
+  console.log(files);
+
+  //let archivo = fs.readFileSync('./src/texto.txt', 'utf-8')
+  fs.readFile('./src/texto.txt', 'utf-8', (error, file) => {
+    if (error) {
+      throw error
+    }
+    console.log(file);
+  })
+  console.log('Leyendo archivo...');
+})
